5eea2892d88986446091aeb2767d8a131ac94efa
Thomas Brodin <info@hotelexistence.com>
2014-02-16 13:20:47 -0500

    Photo size callback

--- content/themes/TB/js/site.js
+++ content/themes/TB/js/site.js
@@ -2,8 +2,8 @@ jQuery(document).ready(function($) {
 
 	$.getJSON("http://diary.thomasbrodin.com/api/read/json?callback=?", function(data) {
 		$.each(data.posts, function(i,posts){
-		var photo500 = this["photo-url-500"];
-		var div_data =  "<img src='"+photo500+"'>";
+		var photo = this["photo-url-1280"];
+		var div_data =  "<img src='"+photo+"'>";
 		$(div_data).appendTo("#teaser");
 		});
 	});
